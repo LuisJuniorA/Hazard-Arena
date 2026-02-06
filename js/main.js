2321e5106f349e7e83321fe9e0de70e133f25002
tt201955 <tristan.tronconi@etu.univ-cotedazur.fr>
2026-02-06 16:07:13 +0100

    implementation systeme gestion de Vues

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 import Player from './class/Player.js';
+import ViewRenderer from './methods/ViewRenderer.js';
+import Level from './class/Level.js';
 
 window.onload = init;
 
@@ -8,63 +10,64 @@ const timeIncr = 1/60;
 let backgroundImage = new Image();
 backgroundImage.src = './assets/background_map/map1_background.png';
 let player;
+let viewRenderer;
 const keys = {};
 
 function init() {
     canvas = document.getElementById('canvas');
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
     ctx = canvas.getContext('2d');
-    ctx.imageSmoothingEnabled = false;
-
+    ctx.imageSmoothingEnabled = true;
+    ctx.imageSmoothingQuality = 'high';
     player = new Player(canvas.width / 2, canvas.height / 2);
+    viewRenderer = new ViewRenderer(ctx,this.levels = {
+            map1: new Level('Forest','./assets/background_map/map1_background.png'),
+            map2: new Level('Wasteworld', './assets/background_map/map2_background.png'),
+            map3: new Level('Snow', './assets/background_map/map3_background.png'),
+            map4: new Level('Complex', './assets/background_map/map4_background.png')
+        },player);
 
+    window.addEventListener('keydown', (e) => {
+        keys[e.key] = true;
+    });
+
+    window.addEventListener('keyup', (e) => {
+        keys[e.key] = false;
+    });
+
+    window.addEventListener('click', (e) => {
+        if (viewRenderer.currentView === 'menu') {
+            if (viewRenderer.clickingButtonHandler(e.clientX, e.clientY)) {
+                viewRenderer.loadMap(clickedButton.map);
+            }   
+        }
+    });
+
+    window.addEventListener('mousemove', (e) => {
+        if (viewRenderer.currentView === 'menu') {
+            viewRenderer.hoveringButtonHandler(e.clientX, e.clientY);
+        }
+    });
     animationLoop();
-}
+}    
+
 
 function animationLoop(time) {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    
+
     render();
     update();
 
     requestAnimationFrame(animationLoop);
 }
 
-
 function render() {
-    const cameraX = player.x - canvas.width / 2;
-    const cameraY = player.y - canvas.height / 2;
-
-    const bgW = backgroundImage.width;
-    const bgH = backgroundImage.height;
-
-    const startX = - (cameraX % bgW);
-    const startY = - (cameraY % bgH);
-
-    for (let x = startX - bgW; x < canvas.width; x += bgW) {
-        for (let y = startY - bgH; y < canvas.height; y += bgH) {
-            ctx.drawImage(backgroundImage, x, y);
-        }
-    }
-
-    //drawEntities();
-
-    ctx.fillStyle = 'red';
-    ctx.fillRect(
-        canvas.width / 2 - 10,
-        canvas.height / 2 - 10,
-        20,
-        20
-    );
-    
-    //les personnages et background ont tous leu vraie position + position x y du joueur pour que tout soit centré sur le joueur
-    /*The player, the map, and all entities have an (x, y) position; however, since the camera is centered on the player, 
-    rendering is done by offsetting every object’s position by the player’s coordinates. As a result, the player’s (x, y) position is mostly conceptual, 
-    as the player does not truly move—only the relative positions within the map change.*/
+    viewRenderer.render();
 }
 
 function update() {
-    //mouvements et collisions
-
+    //mouvements and collisions
     playerMovement();
 }
 let pastKeys = {};
@@ -83,17 +86,9 @@ function playerMovement() {
     if (keys['ArrowRight'] || keys['d']) {
         dx += 1;
     }
-    player.move(dx+px, dy+py);
+    player.move(dx, dy);
     if (JSON.stringify(keys) !== JSON.stringify(pastKeys)) {
         //console.log(keys); //debug keys
         pastKeys = JSON.parse(JSON.stringify(keys));
     }
 }
-
-window.addEventListener('keydown', (e) => {
-    keys[e.key] = true;
-});
-
-window.addEventListener('keyup', (e) => {
-    keys[e.key] = false;
-});
