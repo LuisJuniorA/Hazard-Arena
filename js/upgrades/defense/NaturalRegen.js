db86d79e30488a4de3577246676246c17127a730
tt201955 <tristan.tronconi@etu.univ-cotedazur.fr>
2026-02-15 17:23:46 +0100

    correction de bug

--- js/upgrades/defense/NaturalRegen.js
+++ js/upgrades/defense/NaturalRegen.js
@@ -22,6 +22,6 @@ export default class NaturalRegen extends Upgrade {
     }
 
     getDescription() {
-        return `Restaure ${NaturalRegen.bonus[this.level]} points de vie toute les ${player.healthRegenInterval} secondes.`;
+        return `Restaure ${NaturalRegen.bonus[this.level]} points de vie toute les 10 secondes.`;
     }
 }
